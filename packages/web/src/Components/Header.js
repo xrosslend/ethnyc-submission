3bddda7b5084d8365369188efc733af06efe419c
Taiju Sanagi <taijusanagi@gmail.com>
2022-06-25 22:22:20 -0400

    HOTFIX

--- packages/web/src/Components/Header.js
+++ packages/web/src/Components/Header.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import About from './Header/About.js'
-import Logo from './Header/Logo.js'
-import Rent from './Header/Rent.js'
-import Lend from './Header/Lend.js'
-import WalletConnect from './Header/WalletConnect.js'
-import './Header.css'
-
-
-
+import React from "react";
+import About from "./Header/About.js";
+import Logo from "./Header/Logo.js";
+import Rent from "./Header/Rent.js";
+import Lend from "./Header/Lend.js";
+import WalletConnect from "./Header/WalletConnect.js";
+// import './Header.css'
 
 const Header = () => {
   return (
     <div>
-        <Logo/>
-        <About/>
-        <Rent/>
-        <Lend/>
-        <WalletConnect/>
+      <Logo />
+      <About />
+      <Rent />
+      <Lend />
+      <WalletConnect />
     </div>
-  )
-}
+  );
+};
 
-export default Header
-
+export default Header;
