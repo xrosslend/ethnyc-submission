35620240c4c3d42fff63994c4e10eb46af391aa8
Jannikio <theofficialjannik@gmail.com>
2022-06-25 22:37:42 -0400

    added DatePicker

--- packages/web/src/Components/Main/Rent/RentDuration.js
+++ packages/web/src/Components/Main/Rent/RentDuration.js
@@ -5,8 +5,11 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const RentDuration = () => {
+  const [startDate, setStartDate] = useState(new Date());  
   return (
-    <div>RentDuration</div>
+    <div>
+       <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    </div>
   )
 }
 
